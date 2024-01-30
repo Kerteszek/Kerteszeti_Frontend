cb2aac3b6ba8e6480209c9c3c818e41761611016
Hidy97 <csaladi.alexandra@gmail.com>
2024-01-30 15:39:04 +0100

    Navigáció rendezés

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -3,8 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import './Layout.css';
 import indexKep from '../kepek/indexKep.png';
+import './Layout.css';
 
 const Layout = () => {
     return (
@@ -12,7 +12,7 @@ const Layout = () => {
             <header>
                 <div className="rightSide"><p>Ásó, kapa,</p></div>
                 <div id="indexkep">
-                <img src={indexKep} alt="itt lesz egy kép" /></div>
+                    <img src={indexKep} alt="itt lesz egy kép" /></div>
                 <div><p>harangvirág...</p></div>
             </header>
 
@@ -28,15 +28,11 @@ const Layout = () => {
                             <Nav.Link href="/main">Főoldal</Nav.Link>
 
                             <NavDropdown title="Webshop" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="/public">Webshop</NavDropdown.Item>
-                                <NavDropdown.Item href="/rendez">
-                                    Rendez
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="/valaszt">Választ</NavDropdown.Item>
+                                <NavDropdown.Item href="/public">Aktualitások</NavDropdown.Item>
+                                <NavDropdown.Item href="/rendez">Növények</NavDropdown.Item>
+                                <NavDropdown.Item href="/valaszt">Virágok</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="/">
-                                    Separated link
-                                </NavDropdown.Item>
+                                <NavDropdown.Item href="/">Akciók</NavDropdown.Item>
                             </NavDropdown>
                             <Nav.Link href="/AboutUs">Rólunk</Nav.Link>
                         </Nav>
@@ -47,12 +43,28 @@ const Layout = () => {
                 </Container>
             </Navbar>
 
-            <aside id="asBal">Bal oldal</aside>
+            <aside id="asBal">
+                <h2>Bal oldal</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus facere reprehenderit error, ratione nisi consectetur quaerat quae. Sapiente magni perferendis ullam debitis porro consequuntur delectus excepturi, a at saepe. Quia?
+                    Fugit unde vero tempora eos temporibus iusto facilis mollitia cum assumenda adipisci neque minima aperiam quas, culpa, qui maxime ipsum accusantium quisquam, tempore laborum rem! Et quisquam quis aspernatur asperiores?
+                    Ad, magnam consequuntur maxime temporibus tempore possimus quas deleniti vitae similique minima sint voluptate labore expedita minus libero corporis corrupti cum distinctio nemo totam reiciendis doloremque nulla. Iste, nobis libero!
+                    Aliquid nam voluptatum pariatur consequuntur minus asperiores, aperiam laudantium natus ducimus consequatur laboriosam assumenda ipsam similique eaque suscipit repudiandae fugit illum. Non dolorum consequatur tempore tempora excepturi fuga eum quis.
+                    Inventore, asperiores! Possimus corporis nihil, magni officia ratione delectus expedita recusandae. Minus quo sit nemo doloribus quisquam quas voluptate est nostrum eos, provident dolorum, aliquam incidunt et nulla, iure suscipit.
+                </p>
+            </aside>
             <article>
                 {/* Ide kerül majd az útvonalak/linkek által meghatározott tartalom */}
                 <Outlet />
             </article>
-            <aside id="asJobb">Jobb oldal</aside>
+            <aside id="asJobb">
+                <h2>Jobb oldal</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus facere reprehenderit error, ratione nisi consectetur quaerat quae. Sapiente magni perferendis ullam debitis porro consequuntur delectus excepturi, a at saepe. Quia?
+                    Fugit unde vero tempora eos temporibus iusto facilis mollitia cum assumenda adipisci neque minima aperiam quas, culpa, qui maxime ipsum accusantium quisquam, tempore laborum rem! Et quisquam quis aspernatur asperiores?
+                    Ad, magnam consequuntur maxime temporibus tempore possimus quas deleniti vitae similique minima sint voluptate labore expedita minus libero corporis corrupti cum distinctio nemo totam reiciendis doloremque nulla. Iste, nobis libero!
+                    Aliquid nam voluptatum pariatur consequuntur minus asperiores, aperiam laudantium natus ducimus consequatur laboriosam assumenda ipsam similique eaque suscipit repudiandae fugit illum. Non dolorum consequatur tempore tempora excepturi fuga eum quis.
+                    Inventore, asperiores! Possimus corporis nihil, magni officia ratione delectus expedita recusandae. Minus quo sit nemo doloribus quisquam quas voluptate est nostrum eos, provident dolorum, aliquam incidunt et nulla, iure suscipit.
+                </p>
+            </aside>
             <footer>Készítette: Dreilinger Vanessza Maja és Családi Alexandra</footer>
             <Outlet />
         </>
