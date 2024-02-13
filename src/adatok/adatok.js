575e04b5c0e68b79d30ee65ab9c53191b6ebd87b
DVMaja <majadreilinger@gmail.com>
2024-02-13 11:43:53 +0100

    próba adatok, képek

---
+++ src/adatok/adatok.js
@@ -0,0 +1,27 @@
+export const TERMEKLISTA = [
+    {
+        kode: 1, scientific_name: "Pelargonium peltatum", unit: 1, price: 12000, in_stock: 12, kep: "virage1.png"
+    },
+    {
+        kode: 2, scientific_name: "Pelargonium peltatum", unit: 2, price: 1040, in_stock: 17, kep: "virage2.png"
+    },
+    {
+        kode: 3, scientific_name: "Pelargonium peltatum", unit: 3, price: 1340, in_stock: 56, kep: "virage3.png"
+    },
+    {
+        kode: 4, scientific_name: "Viola x wittrockiana", unit: 1, price: 5400, in_stock: 100, kep: "virage4.png"
+    },
+
+    {
+        kode: 5, scientific_name: "Pelargonium peltatum", unit: 1, price: 12000, in_stock: 12, kep: "virage1.png"
+    },
+    {
+        kode: 6, scientific_name: "Pelargonium peltatum", unit: 2, price: 1040, in_stock: 17, kep: "virage2.png"
+    },
+    {
+        kode: 7, scientific_name: "Pelargonium peltatum", unit: 3, price: 1340, in_stock: 56, kep: "virage3.png"
+    },
+    {
+        kode: 8, scientific_name: "Viola x wittrockiana", unit: 1, price: 5400, in_stock: 100, kep: "virage4.png"
+    },
+];
