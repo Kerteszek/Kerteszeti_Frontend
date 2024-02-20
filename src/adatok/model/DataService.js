50c8c4d92c53577cf31e6b6aa3f82f32a0a1738f
DVMaja <majadreilinger@gmail.com>
2024-02-20 11:48:24 +0100

    Daservice segéd linkek

--- src/adatok/model/DataService.js
+++ src/adatok/model/DataService.js
@@ -1,5 +1,8 @@
+/**https://www.freecodecamp.org/news/how-to-use-axios-with-react/#how-to-set-up-axios-with-react */
+/**https://www.knowledgehut.com/blog/web-development/axios-in-react#why-do-we-need-axios-in-react?%C2%A0 */
+
 class DataService {
-    
+
 
 }
 export default DataService;
