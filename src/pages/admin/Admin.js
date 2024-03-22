8336d0074719d3690d8df7c2e72e69fa9fa3e8cb
Családi Alexandra (SZF_2022_2024) <csaladi.alexandra@diak.szamalk-szalezi.hu>
2024-03-22 13:12:35 +0100

    Admin alapok, elrendezés II. kártyákkal és gombokkkal

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,39 +1,38 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './Admin.css';
 
 export default function Admin() {
     return (
         <div className="admin">Csak belépéssel....
             <section>
-                <Card style={{ width: '14rem' }}>
+                <Card>
                     <Card.Body>
                         <Card.Title>Módosítás</Card.Title>
                         <Card.Text>
                             Itt tudod módosítani az termékeket.
                         </Card.Text>
-                        <Button variant="primary">Termékek módosítása</Button>
+                        <Button variant="primary" id='termod'>Termékek módosítása</Button>
                     </Card.Body>
                 </Card>
-            </section>
-            <section>
-                <Card style={{ width: '14rem' }}>
+
+                <Card>
                     <Card.Body>
                         <Card.Title>Beszerzés</Card.Title>
                         <Card.Text>
                             Új termékek érkeztek! Itt felviheted.
                         </Card.Text>
-                        <Button variant="primary">Termékek felivtele</Button>
+                        <Button variant="primary" id='termfel'>Termékek felivtele</Button>
                     </Card.Body>
                 </Card>
-            </section>
-            <section>
-                <Card style={{ width: '14rem' }}>
+
+                <Card>
                     <Card.Body>
                         <Card.Title>Felhasználók</Card.Title>
                         <Card.Text>
                             Itt módosíthatod a felhasználókat.
                         </Card.Text>
-                        <Button variant="primary">Felhasználók módosítása</Button>
+                        <Button variant="primary" id='fhmod'>Felhasználók módosítása</Button>
                     </Card.Body>
                 </Card>
             </section>
