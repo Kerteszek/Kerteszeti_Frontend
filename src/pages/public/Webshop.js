4c68636eb26965f2a4986097d011368a1f317b1e
DVMaja <majadreilinger@gmail.com>
2024-05-06 23:12:11 +0200

    keresés működik

--- src/pages/public/Webshop.js
+++ src/pages/public/Webshop.js
@@ -1,67 +1,79 @@
-//import { TERMEKLISTA } from "../../adatok/adatok";
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import '../../pages/Layout.css';
 import TermekKartya from '../../components/public/webshop/TermekKartya.js';
-import './Webshop.css';
 import { useNavigate } from 'react-router';
 import { useGet } from "../../model/Axios";
-import Kereses from '../../components/public/Kereses.js';
 import Hirdetes from '../../components/public/Hirdetes.js';
 
-
 export default function Webshop() {
-    // const termekData = Axios({ endpoint: "products" });//Fontos így kell kinéznie!! Régi változat
+    const [searchQuery, setSearchQuery] = useState('');
     const termekData = useGet(`product_w_pictures`);
     const navigate = useNavigate();
-    let dataToPass = {};//obj
+    let dataToPass = {};
 
-    function megnyit(index) {
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    const megnyit = (index) => {
         dataToPass.termek_id = index;
-
-        navigate('/termekOldal', { state: dataToPass});
-
-    }
+        navigate('/termekOldal', { state: dataToPass });
+    };
 
     if (!termekData) {
-        return <div><h1>Kis türelmet.</h1>
-            <h3>A termékek betöltése folyamatban.</h3>
-        </div>;
+        return (
+            <div>
+                <h1>Kis türelmet.</h1>
+                <h3>A termékek betöltése folyamatban.</h3>
+            </div>
+        );
     } else {
+        const filteredData = termekData.filter((elem) => {
+            const scientificName = elem.scientific_name.toLowerCase();
+            const name = elem.name.toLowerCase();
+            const query = searchQuery.toLowerCase();
+            return scientificName.includes(query) || name.includes(query);
+        });
 
         return (
             <div className='row'>
-            <Kereses />
-            <div className="webshop row mx-auto d-flex align-items-center justify-content-center  col-md-8 col-sm-10">
-
-                {termekData.map((elem) => (
-                    <TermekKartya
-                        adatok={termekData}
-                        src={elem.picture_path}
-                        name={elem.name}
-                        ar={elem.price}
-                        tipus={elem.unit}
-                        color={elem.color}
-                        key={elem.product_id}
-                        termek_id={elem.product_id}
-                        megnyit={megnyit}
-                    />
-                ))}
-            </div>
+                <div id="asBal" className="col-md-2 col-sm-1">
+                    Részletes keresés
+                    <Form inline onSubmit={(e) => e.preventDefault()}>
+                        <Row>
+                            <Col xs="auto">
+                                <Form.Control
+                                    type="text"
+                                    placeholder="keresés"
+                                    className="mr-sm-2"
+                                    value={searchQuery}
+                                    onChange={handleSearchChange}
+                                />
+                            </Col>
+                        </Row>
+                    </Form>
+                </div>
+                <div className="webshop row mx-auto d-flex align-items-center justify-content-center  col-md-8 col-sm-10">
+                    {filteredData.map((elem) => (
+                        <TermekKartya
+                            adatok={termekData}
+                            src={elem.picture_path}
+                            name={elem.name}
+                            ar={elem.price}
+                            tipus={elem.unit}
+                            color={elem.color}
+                            key={elem.product_id}
+                            termek_id={elem.product_id}
+                            megnyit={megnyit}
+                        />
+                    ))}
+                </div>
                 <Hirdetes />
             </div>
         );
     }
 }
-
-/** <TermekKartya
-     adatok={termekData}
-     src={elem.picture_path}
-     sname={elem.scientific_name}
-     name={elem.name}
-     dbSzam={elem.in_stock}
-     ar={elem.price}
-     tipus={elem.unit}
-     color={elem.color}
-     key={elem.product_id}
-     termek_id={elem.product_id}
-     megnyit={megnyit}
-                    /> */
